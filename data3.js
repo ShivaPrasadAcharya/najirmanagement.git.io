a2f502f0de9568cf34e082ad105c9db8523b143a
ShivaPrasadAcharya <shivaprasadacharya000000000000@gmail.com>
2025-06-19 19:27:38 +0545

    updateddata3.js

--- data3.js
+++ data3.js
@@ -1,6 +1,6 @@
 // data3.js - Links Dataset
 var data3 = `ID,Name,Remark,Link
-001,RajuNath Pandey,Interim Order-HC Patan,Active,https://www.ratopati.com/story/496043/high-court-says-in-raju-pandeys-writ-petition-dont-implement-suspension-let-him-work
+001,RajuNath Pandey,Interim Order-HC Patan,https://www.ratopati.com/story/496043/high-court-says-in-raju-pandeys-writ-petition-dont-implement-suspension-let-him-work
 002,Elephant Killing,Ecosystem Balancing,https://www.onlinekhabar.com/2025/06/1703980/zimbabwe-plans-to-kill-elephants-and-distribute-meat-to-the-public
 008,Project Theta,65000,Pending,Resource allocation needed`;
 
